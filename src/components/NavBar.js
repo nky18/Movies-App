4d2ef9cb21dfa98b9295d9c4e41dd641d1624761
NAVIN KUMAR YADAV <100862632+nky18@users.noreply.github.com>
2022-07-28 00:38:40 +0530

    Add files via upload

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,9 @@ import{ Link } from "react-router-dom";
 class NavBar extends Component{
     render(){
         return(
-        <div style={{display:"flex",padding:'1rem',color:"blue"}}>
+        <div style={{display:"flex",padding:'1rem',color:"#1f6dd4"}}>
             <Link to="/" style={{textDecoration:"none"}}><h1>Movies App</h1></Link>
-            <Link to="/Favourites" style={{textDecoration:"none"}}><h2 style={{marginLeft:"2rem",marginTop:"1rem"}}>Favourites</h2> </Link>
+            <Link to="/Favourites" style={{textDecoration:"none"}}><h2 style={{marginLeft:"2rem",marginTop:"0.5rem"}}>Favourites</h2> </Link>
             
         </div>
         )
